8db24db05f83b2fb921b87a057cd29ed63e9a0a7
toshi-g <toshiyuki000@gmail.com>
2019-06-30 23:34:51 +0900

    完成

--- vali.php
+++ vali.php
@@ -1,5 +1,16 @@
 <?PHP
 //ポストの内容を取得し、入力内容が正しいかチェックする。
+$movie = "";
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+    $movie = $_POST['movie'];
+
+    if(mb_strlen($movie) === 0){
+        $err = "文字を入力してください";
+    }elseif(mb_strlen($movie) > 20){
+        $err = "20文字以内で入力してください";
+    }
+}
 ?>
 
 <html>
@@ -15,6 +26,11 @@ input{margin:5px}
 <p>
 <?php
 //誤りがあれば、エラー、正しければ、「あなたの好きな映画：」
+if(isset($err)){
+    echo $err;
+}else{
+    echo "あなたの好きな映画は：".$movie;
+}
 ?>
 </p>
 <form action="" method="POST">
