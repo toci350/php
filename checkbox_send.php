047e51ea6aafcfab78c251b9a7057ae4bded55c8
toshi-g <toshiyuki000@gmail.com>
2019-07-01 05:07:55 +0900

    配列

---
+++ checkbox_send.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h1>チェックボックスを使ったフォーム</h1>
+<p>好きな色を選択（複数選択可）</p>
+<form action="ch_re.php" method="POST">
+<p>
+<input type="checkbox" name="colors[]" value="青">青
+<input type="checkbox" name="colors[]" value="赤">赤
+<input type="checkbox" name="colors[]" value="黄">黄
+<input type="checkbox" name="colors[]" value="緑">緑
+<input type="checkbox" name="colors[]" value="紫">紫
+<input type="checkbox" name="colors[]" value="白">白
+<input type="checkbox" name="colors[]" value="橙">橙
+</p>
+</form>
+</body>
+</html>
