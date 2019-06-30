2d76b2dfe944b2c101a8c8534d34cf4a36fd87d7
toshi-g <toshiyuki000@gmail.com>
2019-07-01 02:52:38 +0900

    生年月日を入力するフォーム

---
+++ birth_form.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<h1>生年月日を入力するフォーム</h1>
+<label for="year">西暦</label>
+<select name="year">
+<option value="1980">1980</option>
+<option value="1981">1981</option>
+</select>年
+<br>
+<select name="month">
+<option value="1">1</option>
+<option value="2">2</option>
+</select>月
+<select name="day">
+<option value="1">1</option>
+<option value="2">2</option>
+</select>日
+</body>
+</html>
