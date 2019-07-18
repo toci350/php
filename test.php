a825e1dd9e6be709bca0a750739e129f3f2a1465
toshi-g <toshiyuki000@gmail.com>
2019-07-18 15:41:23 +0900

    form

--- test.php
+++ test.php
@@ -1,1 +1,15 @@
-<?php phpinfo(); ?>
+<html>
+<head>
+    <title>personalized greeting form</title>
+</head>
+<body>
+    <?php if(!empty($_POST['name'])) {
+      echo "greeting, {$_POST['name']}, and welcome";
+    } ?>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        enter your name: <input type="text" name="name"/>
+        <input type="submit"/>
+    </form>
+</body>
+</html>
